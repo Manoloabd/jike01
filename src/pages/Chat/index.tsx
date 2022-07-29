712d4414c2dee5e9fd0354dc7a014751a7c3cccc
user1 <user1@lab3.com>
2022-07-29 13:58:31 +0800

    小智同学创建Socket

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -3,10 +3,46 @@ import classnames from 'classnames'
 import { useHistory } from 'react-router-dom'
 import Icon from '@/components/Icon'
 import styles from './index.module.scss'
-
+import { io } from 'socket.io-client'
+import { useEffect, useState } from 'react'
+import { getToken } from '@/utils/auth'
+type ChatRecord = {
+  type: 'xz' | 'user'
+  message: string
+}
 const Chat = () => {
   const history = useHistory()
-
+  const [chatList, setChatList] = useState<ChatRecord[]>([])
+  const [value, setValue] = useState('')
+  //  建立通讯链接
+  useEffect(() => {
+    const socketIO = io('http://toutiao.itheima.net', {
+      query: {
+        token: getToken().token,
+      },
+      transports: ['websocket'],
+    })
+    // 监听connect事件
+    socketIO.on('connect', () => {
+      console.log('和极客园小智同学连接成功')
+      setChatList([
+        {
+          type: 'xz',
+          message: '欢迎来到聊天室',
+        },
+        {
+          type: 'xz',
+          message: '请说点什么吧',
+        },
+      ])
+    })
+    socketIO.on('disconnect', () => {
+      console.log('和极客园小智同学断开连接')
+    })
+    return () => {
+      socketIO.close()
+    }
+  }, [])
   return (
     <div className={styles.root}>
       <NavBar className='fixed-header' onBack={() => history.go(-1)}>
@@ -14,18 +50,37 @@ const Chat = () => {
       </NavBar>
 
       <div className='chat-list'>
-        <div className={classnames('chat-item', true ? 'self' : 'user')}>
-          {true ? (
-            <Icon type='iconbtn_xiaozhitongxue' />
-          ) : (
-            <img src='http://geek.itheima.net/images/user_head.jpg' alt='' />
-          )}
-          <div className='message'>你好，我是小智</div>
-        </div>
+        {/* 循环生成聊天记录 */}
+        {chatList.map((item, index) => {
+          return (
+            <div
+              className={classnames(
+                'chat-item',
+                item.type === 'xz' ? 'self' : 'user'
+              )}
+              key={index}
+            >
+              {item.type === 'xz' ? (
+                <Icon type='iconbtn_xiaozhitongxue' />
+              ) : (
+                <img
+                  src='http://geek.itheima.net/images/user_head.jpg'
+                  alt=''
+                />
+              )}
+              <div className='message'>{item.message}</div>
+            </div>
+          )
+        })}
       </div>
 
       <div className='input-footer'>
-        <Input className='no-border' placeholder='请描述您的问题' />
+        <Input
+          className='no-border'
+          placeholder='请描述您的问题'
+          value={value}
+          onChange={setValue}
+        />
         <Icon type='iconbianji' />
       </div>
     </div>
