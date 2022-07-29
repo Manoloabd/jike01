0914e26e29af2a693c711f56751c0fcc8aee6cf4
user1 <user1@lab3.com>
2022-07-29 14:01:08 +0800

    喝小智进行通讯

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -3,8 +3,8 @@ import classnames from 'classnames'
 import { useHistory } from 'react-router-dom'
 import Icon from '@/components/Icon'
 import styles from './index.module.scss'
-import { io } from 'socket.io-client'
-import { useEffect, useState } from 'react'
+import { io, Socket } from 'socket.io-client'
+import { useEffect, useState, useRef } from 'react'
 import { getToken } from '@/utils/auth'
 type ChatRecord = {
   type: 'xz' | 'user'
@@ -14,6 +14,7 @@ const Chat = () => {
   const history = useHistory()
   const [chatList, setChatList] = useState<ChatRecord[]>([])
   const [value, setValue] = useState('')
+  const socketRef = useRef<Socket>() // 使用ref指定泛型类型
   //  建立通讯链接
   useEffect(() => {
     const socketIO = io('http://toutiao.itheima.net', {
@@ -24,6 +25,7 @@ const Chat = () => {
     })
     // 监听connect事件
     socketIO.on('connect', () => {
+      socketRef.current = socketIO // 在连接成功的时候 赋值给ref的current属性
       console.log('和极客园小智同学连接成功')
       setChatList([
         {
@@ -36,13 +38,40 @@ const Chat = () => {
         },
       ])
     })
+    socketIO.on('message', (data) => {
+      // 小智回了消息
+      setChatList((list) => [...list, { message: data.msg, type: 'xz' }])
+      // setChatList()
+      console.log(chatList)
+    })
     socketIO.on('disconnect', () => {
       console.log('和极客园小智同学断开连接')
     })
     return () => {
       socketIO.close()
     }
-  }, [])
+  }, [chatList])
+  // 定义发送消息的一个方法
+  const sendMessage = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    // 当键盘按下时 触发
+    if (event.code === 'Enter' && !!value.trim()) {
+      // 此时表示 既回车 又有内容 可以直接发送
+      socketRef.current?.emit('message', {
+        msg: value.trim(),
+        timestamp: Date.now().toString(),
+      })
+      // 将自己的说的话 添加聊天内容
+      setChatList([
+        ...chatList,
+        {
+          type: 'user',
+          message: value.trim(),
+        },
+      ])
+      // 此时消息发完了
+      setValue('') // 清空输入框
+    }
+  }
   return (
     <div className={styles.root}>
       <NavBar className='fixed-header' onBack={() => history.go(-1)}>
@@ -77,6 +106,7 @@ const Chat = () => {
       <div className='input-footer'>
         <Input
           className='no-border'
+          onKeyDown={sendMessage}
           placeholder='请描述您的问题'
           value={value}
           onChange={setValue}
